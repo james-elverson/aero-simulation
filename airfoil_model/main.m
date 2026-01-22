cfbb3853ae3c4c0c502f1d47e543c9a42fb09c30
James Elverson <jameselverson813@gmail.com>
2026-01-22 15:22:47 -0500

    Add main.m for airfoil model

---
+++ airfoil_model/main.m
@@ -0,0 +1,10 @@
+p.alpha_deg = -10;
+p.camber_u = 0.04;
+p.camber_l = 0.00;
+p.rho = 1.225;
+p.V = 30;
+p.chord = 0.25;
+p.span = 1.0;   % 2D: per meter span
+
+out = airfoilLiftBasic(p);
+disp(out)
